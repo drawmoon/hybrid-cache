296344dc80b3d333fa283e6b79f4a92a09a0a0c1
drsh <1340260725@qq.com>
2023-06-26 18:08:54 +0800

    调整依赖引用关系，新增在构建时生成 javadoc 和 jar 打包

--- hybridobs/src/main/java/org/hybridcache/hybridobs/HybridStorePlace.java
+++ hybridobs/src/main/java/org/hybridcache/hybridobs/HybridStorePlace.java
@@ -5,17 +5,17 @@ package org.hybridcache.hybridobs;
  */
 public enum HybridStorePlace {
     /**
-     * 表示 {@link HybridOBS} 应该自动选择缓存位置。
+     * 表示 {@link HybridStore} 应该自动选择缓存位置。
      */
     AUTO,
 
     /**
-     * 表示 {@link HybridOBS} 应该将对象存储在本地磁盘中。
+     * 表示 {@link HybridStore} 应该将对象存储在本地磁盘中。
      */
     LOCAL,
 
     /**
-     * 表示 {@link HybridOBS} 应该将对象分布在多个节点上。
+     * 表示 {@link HybridStore} 应该将对象分布在多个节点上。
      */
     DISTRIBUTED
 }
